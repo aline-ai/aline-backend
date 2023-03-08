2cb51f3d38dd56128ed366f6f0fd653c9e4b4cf8
= <kevinlu1248@gmail.com>
2023-03-07 19:16:29 -0800

    minor fix to end token

--- src/autocomplete.py
+++ src/autocomplete.py
@@ -4,7 +4,8 @@ import mistletoe
 import html
 
 from langchain.embeddings.openai import OpenAIEmbeddings, Embeddings
-from langchain.vectorstores import Chroma,  VectorStore
+from langchain.vectorstores import Pinecone, VectorStore
+# from langchain.vectorstores import Chroma,  VectorStore
 # from langchain.vectorstores import Qdrant
 from langchain.chains import VectorDBQA
 from langchain.llms import OpenAI, OpenAIChat
@@ -18,14 +19,16 @@ load_dotenv()
 Document = Any
 
 class LinearSearchVectorStore(VectorStore):
-    texts: list[str] = 0
+    embeddings: Embeddings 
+    texts: list[str] = []
+    vectors: list[list[float]] = []
 
     def add_texts(self, texts: Iterable[str], metadatas: list[dict] | None = None, **kwargs: Any) -> list[str]:
         self.texts.extend(texts)
         return list(len(self.texts) - len(texts), range(len(self.texts)))
     
     def similarity_search(self, query: str, k: int = 4, **kwargs: Any) -> list[Document]:
-        pass
+        vector = self.embeddings.embed_query(query)
 
     def max_marginal_relevance_search(self, query: str, k: int = 4, fetch_k: int = 20) -> list[Document]:
         # return super().max_marginal_relevance_search(query, k, fetch_k) 
@@ -41,7 +44,9 @@ class LinearSearchVectorStore(VectorStore):
         metadatas: list[dict] | None = None,
         **kwargs: Any,
     ) -> VectorStore:
-        pass
+        self.embeddings = embedding
+        self.add_documents(texts, metadatas)
+        return self
 
 
 CURSOR_INDICATOR = " CURSOR_INDICATOR"
@@ -61,8 +66,9 @@ def autocomplete(_url, context, notes):
         # Refactor this mess
         embeddings = OpenAIEmbeddings()
         # TODO: Use Qdrant or something that doesn't require like an hour to build
-        # docsearch = Qdrant.from_documents(documents, embeddings)
-        docsearch = Chroma.from_documents(documents, embeddings)
+        # docsearch = Qdrant.from_documents(documents, embeddings, host="https://fbf55977-79ae-48c2-a691-74ce7b589764.us-east-1-0.aws.cloud.qdrant.io", api_key="L47voMiazaOrenR-Dm9SWwxTBGIoS0LU5LqLYMo2V9PRtfqRtIx_Iw")
+        # docsearch = Chroma.from_documents(documents, embeddings)
+        docsearch = Pinecone.from_documents(documents, embeddings, index_name="aline", api_key="1a8e5e99-115c-4ab8-814e-4cfab13efb17")
         qa = VectorDBQA.from_chain_type(
             llm=llm, 
             chain_type="map_reduce", 
@@ -77,6 +83,8 @@ def autocomplete(_url, context, notes):
     else:
         prompt = load_prompt("src/prompts/autocomplete/reduce.yaml")
         completion = llm(prompt.format(question=notes_in_md, summaries=context_in_md))
+    
+    completion, _ = completion.split("===END===")
 
     # Postprocess the prompt to return output html
     completed_notes = notes_in_md + CURSOR_INDICATOR + completion
